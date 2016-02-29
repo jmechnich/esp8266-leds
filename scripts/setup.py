8f16cb74ca21b24d32a5e30338a162cdca6721a0
jmechnich <joerg.mechnich@gmail.com>
2016-02-29 21:51:07 +0100

    added installation script

---
+++ scripts/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='esp8266-leds',
+    version='0.1.0',
+    description='Utilities for controlling WS1812 LED strips with an Espressif ESP8266.',
+    url='https://github.com/jmechnich/esp8266-leds',
+    author='Joerg Mechnich',
+    author_email='joerg.mechnich@gmail.com',
+    license='MIT',
+    scripts=[
+        'ledclient.py',
+    ],
+)
